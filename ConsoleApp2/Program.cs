f00f43ef1a245712863f1ebdc39e0992cce07098
hjhuang <Howard.Jhuang@hermes-microvision.com>
2019-09-18 15:34:57 +0800

    startInfo.Arguments = "/C " + command; startInfo.RedirectStandardOutput = true;

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,22 +11,21 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Execute("echo hello", 10);
-            Execute("cd", 10);
-
+            Console.Write(Execute("echo hello", 10));
+            Console.Write(Execute("cd", 10));
         }
-        public static void Execute(string command, int seconds)
+        public static string Execute(string command, int seconds)
         {
-            
+            string output="";
             if (command != null && !command.Equals(""))
             {
                 Process process = new Process();//创建进程对象  
                 ProcessStartInfo startInfo = new ProcessStartInfo();
                 startInfo.FileName = "cmd.exe";//设定需要执行的命令  
-                startInfo.Arguments = /*"/C " +*/ command;//“/C”表示执行完命令后马上退出  
+                startInfo.Arguments = "/C " + command;//“/C”表示执行完命令后马上退出  
                 startInfo.UseShellExecute = false;//不使用系统外壳程序启动 
                 startInfo.RedirectStandardInput = false;//不重定向输入  
-                startInfo.RedirectStandardOutput = false; //重定向输出  
+                startInfo.RedirectStandardOutput = true; //重定向输出  
                 startInfo.CreateNoWindow = true;//不创建窗口  
                 process.StartInfo = startInfo;
                 try
@@ -41,7 +40,7 @@ namespace ConsoleApp2
                         {
                             process.WaitForExit(seconds); //等待进程结束，等待时间为指定的毫秒  
                         }
-                        
+                        output = process.StandardOutput.ReadToEnd();
                     }
                 }
                 catch (Exception ex)
@@ -53,8 +52,9 @@ namespace ConsoleApp2
                     if (process != null)
                         process.Close();
                 }
+                
             }
-            
+            return output;
         }
     }
 }
