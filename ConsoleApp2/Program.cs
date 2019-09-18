2f70dab9f1e9e40d3faabc7336b93ffb2d733bf6
hjhuang <Howard.Jhuang@hermes-microvision.com>
2019-09-18 15:39:51 +0800

    startInfo.RedirectStandardOutput = false;

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,12 +11,12 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.Write(Execute("echo hello", 10));
-            Console.Write(Execute("cd", 10));
+            Execute("echo hello", 10);
+            Execute("cd", 10);
         }
-        public static string Execute(string command, int seconds)
+        public static void Execute(string command, int seconds)
         {
-            string output="";
+            
             if (command != null && !command.Equals(""))
             {
                 Process process = new Process();//创建进程对象  
@@ -25,7 +25,7 @@ namespace ConsoleApp2
                 startInfo.Arguments = "/C " + command;//“/C”表示执行完命令后马上退出  
                 startInfo.UseShellExecute = false;//不使用系统外壳程序启动 
                 startInfo.RedirectStandardInput = false;//不重定向输入  
-                startInfo.RedirectStandardOutput = true; //重定向输出  
+                startInfo.RedirectStandardOutput = false; //重定向输出  
                 startInfo.CreateNoWindow = true;//不创建窗口  
                 process.StartInfo = startInfo;
                 try
@@ -40,7 +40,7 @@ namespace ConsoleApp2
                         {
                             process.WaitForExit(seconds); //等待进程结束，等待时间为指定的毫秒  
                         }
-                        output = process.StandardOutput.ReadToEnd();
+                        
                     }
                 }
                 catch (Exception ex)
@@ -54,7 +54,7 @@ namespace ConsoleApp2
                 }
                 
             }
-            return output;
+            
         }
     }
 }
